4fcdb5354e7ea5da60e1bb21446e00e067673876
xunyuqianli <274596545@qq.com>
2016-07-29 15:59:57 +0800

    刘元元第三次提交

--- app/src/main/java/com/chalenge/fanyelll/fragment/fragment_tuijian.java
+++ app/src/main/java/com/chalenge/fanyelll/fragment/fragment_tuijian.java
@@ -2,6 +2,8 @@ package com.chalenge.fanyelll.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -26,6 +28,8 @@ import com.squareup.picasso.Picasso;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -36,22 +40,22 @@ import okhttp3.Response;
 /**
  * Created by Administrator on 2016/7/25.
  */
-public class fragment_tuijian  extends Fragment {
+public class fragment_tuijian extends Fragment {
 
-    private static final String TAG ="info--fragment_tuijian" ;
+    private static final String TAG = "info--fragment_tuijian";
     private View lView;
     private ViewPager mViewPager;
     private List<String> imagePaths_viewpager;
-    private String pathhead="http://morguo.com/";
-    private List<ImageView> mImageViewList=new ArrayList<>();
+    private String pathhead = "http://morguo.com/";
+    private List<ImageView> mImageViewList = new ArrayList<>();
     private adapter_viewpager_image lAdapter_viewpager_image;
     private List<First_tuijian_bean.DataBean.ListBean> mList_item;
     private List<First_tuijian_bean.DataBean.ExplorerbannerlistBean> mExplorerbannerlist;
     private MyRecyclerview recyclerview;
     private Adapter_recyclerview adapter_recyclerview;
-    private List<First_tuijian_bean.DataBean.ListBean> list=new ArrayList<>();
-    private String path1="http://morguo.com/forum.php?mod=movieexplorer&v=3&androidflag=1&page=%d&pdateline=1469668475";
-    private int page=1;
+    private List<First_tuijian_bean.DataBean.ListBean> list = new ArrayList<>();
+    private String path1 = "http://morguo.com/forum.php?mod=movieexplorer&v=3&androidflag=1&page=%d&pdateline=1469668475";
+    private int page = 1;
     private String pathOk;
     private boolean isBottom = false;
     private ImageView shangying_to;
@@ -59,20 +63,47 @@ public class fragment_tuijian  extends Fragment {
     private ImageView yingdan_to;
     private ImageView gengduo_to;
 
+    //声明计时器
+    private Timer timer;
+    private int index;
+    //创建handler重写handlermassage方法
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            int index = msg.arg1;
+            if (mImageViewList.size() != 0) {
+                mViewPager.setCurrentItem(index++ % mImageViewList.size());
 
+            }
+        }
+
+
+    };
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        lView=inflater.inflate(R.layout.item_fragment_tuijian,container,false);
-        imagePaths_viewpager=new ArrayList<>();
+        lView = inflater.inflate(R.layout.item_fragment_tuijian, container, false);
+        imagePaths_viewpager = new ArrayList<>();
         initview();
-        pathOk=String.format(path1,page);
+        pathOk = String.format(path1, page);
         initOkhttp(pathOk);
-        lAdapter_viewpager_image=new adapter_viewpager_image(mImageViewList,getContext());
+        lAdapter_viewpager_image = new adapter_viewpager_image(mImageViewList, getContext());
         mViewPager.setAdapter(lAdapter_viewpager_image);
+        timer=new Timer();
+        //定时器每隔2秒发送一次消息
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Message msg = new Message();
+                msg.arg1 = index++;
+                handler.sendMessage(msg);
+            }
+        },0,2000);
+
         final FullyLinearLayoutManager layoutManager = new FullyLinearLayoutManager(getContext());
         recyclerview.setLayoutManager(layoutManager);
-        adapter_recyclerview=new Adapter_recyclerview(list,getContext());
+        adapter_recyclerview = new Adapter_recyclerview(list, getContext());
         recyclerview.setAdapter(adapter_recyclerview);
 //        recyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
 //
@@ -117,21 +148,21 @@ public class fragment_tuijian  extends Fragment {
         shangying_to.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getContext(), ShangyingActivity.class);
+                Intent intent = new Intent(getContext(), ShangyingActivity.class);
                 startActivity(intent);
             }
         });
         duanpian_to.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getContext(), DuanPianActivity.class);
+                Intent intent = new Intent(getContext(), DuanPianActivity.class);
                 startActivity(intent);
             }
         });
         yingdan_to.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getContext(), YingpingActivity.class);
+                Intent intent = new Intent(getContext(), YingpingActivity.class);
                 startActivity(intent);
             }
         });
@@ -141,21 +172,21 @@ public class fragment_tuijian  extends Fragment {
     private void initview() {
         mViewPager = (ViewPager) lView.findViewById(R.id.viewpager_tuijian);
         recyclerview = (MyRecyclerview) lView.findViewById(R.id.recyclerview);
-        shangying_to = (ImageView)lView.findViewById(R.id.imageview_screen_select);
-        duanpian_to = (ImageView)lView.findViewById(R.id.imageview_short_select);
-        yingdan_to = (ImageView)lView.findViewById(R.id.imageview_movielist_select);
-        gengduo_to = (ImageView)lView.findViewById(R.id.imageview_more_select);
+        shangying_to = (ImageView) lView.findViewById(R.id.imageview_screen_select);
+        duanpian_to = (ImageView) lView.findViewById(R.id.imageview_short_select);
+        yingdan_to = (ImageView) lView.findViewById(R.id.imageview_movielist_select);
+        gengduo_to = (ImageView) lView.findViewById(R.id.imageview_more_select);
 
-}
+    }
 
 
     private void initOkhttp(String path) {
 
-        OkHttpClient lOkHttpClient=new OkHttpClient();
-        Request lRequest=new Request.Builder()
+        OkHttpClient lOkHttpClient = new OkHttpClient();
+        Request lRequest = new Request.Builder()
                 .url(path)
                 .build();
-        Call lCall=lOkHttpClient.newCall(lRequest);
+        Call lCall = lOkHttpClient.newCall(lRequest);
         lCall.enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -169,7 +200,7 @@ public class fragment_tuijian  extends Fragment {
                 page++;
                 String str = response.body().string();
 
-                Gson lGson=new Gson();
+                Gson lGson = new Gson();
                 First_tuijian_bean lFirst_tuijian_bean = lGson.fromJson(str, First_tuijian_bean.class);
                 mList_item = lFirst_tuijian_bean.getData().getList();
                 list.addAll(mList_item);
@@ -184,12 +215,9 @@ public class fragment_tuijian  extends Fragment {
 
                 mExplorerbannerlist = lFirst_tuijian_bean.getData().getExplorerbannerlist();
                 for (int lI = 0; lI < mExplorerbannerlist.size(); lI++) {
-                    String path=pathhead+mExplorerbannerlist.get(lI).getImage();
+                    String path = pathhead + mExplorerbannerlist.get(lI).getImage();
                     imagePaths_viewpager.add(path);
-                    Log.i(TAG, "onResponse: "+path);
-
-
-
+                    Log.i(TAG, "onResponse: " + path);
 
 
                 }
@@ -206,8 +234,6 @@ public class fragment_tuijian  extends Fragment {
                 });
 
 
-
-
             }
         });
     }
@@ -217,7 +243,7 @@ public class fragment_tuijian  extends Fragment {
 
         for (int i = 0; i < imagePaths_viewpager.size(); i++) {
 
-            ImageView image=new ImageView(getContext());
+            ImageView image = new ImageView(getContext());
             Picasso.with(getContext())
                     .load(imagePaths_viewpager.get(i))
                     .into(image);
@@ -226,7 +252,6 @@ public class fragment_tuijian  extends Fragment {
         }
 
     }
-
 
 
     public static fragment_tuijian newInstance() {
